5e647fae9a4fc6eb7de0e5ef7ea64e3defc8f3a1
Paul Fell-Cinoman <swellpf@gmail.com>
2018-11-03 00:41:00 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,8 +9,8 @@ class Author
     @posts=[]
   end
  # binding.pry
-  def add_post(post)
-    @posts << post
+  def add_post(new_post)
+    @posts << new_post
     post.author=self
     @@all << post
   end
